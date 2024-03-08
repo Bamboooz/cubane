6ed05f46cfd1b7b217a7189fae2a8d58daa671ad
Bamboooz <bambusixmc@gmail.com>
2024-03-08 16:00:05 +0100

    trying to make the context menu more responsive

--- src/components/common/Context/index.tsx
+++ src/components/common/Context/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import { useOnClickOutside } from "../../../hooks/useOnClickOutside";
 import { cn } from "../../../utils/tw";
@@ -13,11 +13,35 @@ interface ContextProps {
 
 const Context: React.FC<ContextProps> = ({ x, y, closeContextMenu, className, children }) => {
     const contextMenuRef = useRef<HTMLDivElement>(null);
+    const [position, setPosition] = useState({ top: y, left: x });
+
     useOnClickOutside(contextMenuRef, closeContextMenu);
+
+    useEffect(() => {
+        // FIXME: sometimes randomly going up when its not meant to
+        const updatePosition = () => {
+            if (contextMenuRef.current) {
+                const rect = contextMenuRef.current.getBoundingClientRect();
+                const { innerWidth, innerHeight } = window;
+    
+                const spaceAbove = y;
+                const spaceBelow = innerHeight - y;
+                const spaceLeft = x;
+                const spaceRight = innerWidth - x;
+                
+                const newTop = spaceAbove > spaceBelow ? Math.max(0, y - rect.height) : y;
+                const newLeft = spaceLeft > spaceRight ? Math.max(0, x - rect.width) : x;
+    
+                setPosition({ top: newTop, left: newLeft });
+            }
+        };
+    
+        updatePosition();
+    }, [x, y]);
 
     return (
         <>
-            <div ref={contextMenuRef} className={cn("fixed z-30", className)} style={{ top: `${y}px`, left: `${x}px` }}>
+            <div ref={contextMenuRef} className={cn("fixed z-30", className)} style={position}>
                 {children}
             </div>
         </>
